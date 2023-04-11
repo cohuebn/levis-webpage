1eac92651b5842049a955de4e3686dd2dc38992e
Cory Huebner <chuebner86@gmail.com>
2023-04-11 13:20:50 -0500

    levi added pictures; getting the material to play with next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Box, Container, Input, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 export default function Index() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Character picker
+          Mario Picker
         </Typography>
       </Box>
     </Container>
