4be4bcb1a227f82c90fcd065c4842f2d4edd9221
Cory Huebner <chuebner86@gmail.com>
2023-04-26 08:37:53 -0500

    wired up autocomplete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,46 @@
-import { Box, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  Autocomplete,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
+
+type CharacterOption = {
+  label: string;
+};
 
 export default function Index() {
+  const options: CharacterOption[] = [
+    { label: "Wario" },
+    { label: "Wario and Waluigi" },
+    { label: "Yoshi" },
+  ];
+  const [selectedCharacter, selectCharacter] = useState<CharacterOption | null>(
+    null
+  );
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Mario Picker
         </Typography>
+        <Autocomplete
+          id="character name"
+          options={options}
+          renderInput={(params) => (
+            <TextField {...params} label="character name" variant="filled" />
+          )}
+          onChange={(_event: unknown, newCharacter: CharacterOption | null) => {
+            selectCharacter(newCharacter);
+          }}
+        />
+        <p>
+          {selectedCharacter
+            ? selectedCharacter.label
+            : "No character selected"}
+        </p>
       </Box>
     </Container>
   );
