30a169414d0eb7e5d930f44fe415a3eaa93d15d4
Cory Huebner <chuebner86@gmail.com>
2023-06-30 11:14:05 -0500

    add jest for testing and lazy load util for lazy loading

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+// jest.config.js
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+const esModules = ["hashconnect", "uuid"].join("|");
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: [
+    `[/\\\\]node_modules[/\\\\](?!${esModules}).+\\.(js|jsx|mjs|cjs|ts|tsx)$`,
+  ],
+});
